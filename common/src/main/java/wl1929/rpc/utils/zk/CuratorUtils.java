a6429ea6882d2c139595e56d744ae3d493fc3041
wl1929 <wangli4773@163.com>
2020-06-28 15:09:14 +0800

    1

--- common/src/main/java/wl1929/rpc/utils/zk/CuratorUtils.java
+++ common/src/main/java/wl1929/rpc/utils/zk/CuratorUtils.java
@@ -1,5 +1,10 @@
 package wl1929.rpc.utils.zk;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.CreateMode;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -11,6 +16,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * @description
  */
 
+
+@Slf4j
 public class CuratorUtils {
 
     private static final int BASE_SLEEP_TIME = 1000;
@@ -34,5 +41,31 @@ public class CuratorUtils {
      *
      * @param path 节点路径
      */
+    public static void createPersistentNode(String path) {
+        try {
+            if (registeredPathSet.contains(path) || zkClient.checkExists().forPath(path) != null) {
+                log.info("节点已经存在，节点为:[{}]", path);
+            } else {
+                //eg: /my-rpc/github.javaguide.HelloService/127.0.0.1:9999
+                zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path);
+                log.info("节点创建成功，节点为:[{}]", path);
+            }
+            registeredPathSet.add(path);
+        } catch (Exception e) {
+            throw new RpcException(e.getMessage(), e.getCause());
+        }
+    }
 
+    /**
+     * 获取某个字节下的子节点,也就是获取所有提供服务的生产者的地址
+     *
+     * @param serviceName 服务对象接口名 eg:github.javaguide.HelloService
+     * @return 指定字节下的所有子节点
+     */
+    public static List<String> getChildrenNodes(String serviceName) {
+        if (serviceAddressMap.containsKey(serviceName)) {
+            return serviceAddressMap.get(serviceName);
+        }
+        
+    }
 }
